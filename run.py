e5b64fcdeb68689dcfdffa0eb3689423e44e6dfc
primary pigments <primarypigments@gmail.com>
2024-01-23 20:50:10 +0000

    Add scope to auth. google drive, gsheets

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from simple_term_menu import TerminalMenu
 import gspread
 from google.oauth2.service_account import Credentials
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
 
