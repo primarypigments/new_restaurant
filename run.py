586f5b85cf19a522a4e5b6d4daf3e613e048a9c7
primary pigments <primarypigments@gmail.com>
2024-01-23 21:43:55 +0000

    Add credits

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from simple_term_menu import TerminalMenu
 import gspread
 from google.oauth2.service_account import Credentials
+
+# SCOPE scourced from code institue 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ]
-
+# https://pypi.org/project/simple-term-menu/ used for folowwing code
 def show_program_menu():
     program_menu_title = "Select an option:"
     program_menu_items = ["View Statistics", "Add New Restaurant"]
