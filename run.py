1e5f07d449a8321c43ed659794a02e4df60a97c8
primary pigments <primarypigments@gmail.com>
2024-01-23 21:30:34 +0000

    Add class to create, configure, display the menu

--- run.py
+++ run.py
@@ -13,3 +13,8 @@ def show_program_menu():
     program_menu_cursor = " -> "
     program_menu_cursor_style = ("fg_purple", "bold")
     program_menu_style = ("bg_yellow", "fg_gray")
+
+    program_menu = TerminalMenu(program_menu_items, title=program_menu_title, menu_cursor=program_menu_cursor, menu_cursor_style=program_menu_cursor_style, menu_highlight_style= program_menu_style)
+
+    selected_index = program_menu.show()
+    return selected_index
