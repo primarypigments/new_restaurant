2eed9e755ba3debf1394594e66b0e8a30133c166
primary pigments <primarypigments@gmail.com>
2024-01-23 21:25:38 +0000

    Add tuple to set curor style color

--- run.py
+++ run.py
@@ -11,3 +11,4 @@ def show_program_menu():
     program_menu_title = "Select an option:"
     program_menu_items = ["View Statistics", "Add New Restaurant"]
     program_menu_cursor = " -> "
+    program_menu_cursor_style = ("fg_purple", "bold")
