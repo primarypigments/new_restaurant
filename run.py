da11e8a4a182c1d7a7a1fba1ef54d97a0f78906e
primary pigments <primarypigments@gmail.com>
2024-01-25 23:33:41 +0000

    Add comments, format doc, remove redundant code

--- run.py
+++ run.py
@@ -3,13 +3,14 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
-# SCOPE scourced from code institue 
+# SCOPE sourced from code institute
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ]
-# https://pypi.org/project/simple-term-menu/ used for folowwing code
+
+# https://pypi.org/project/simple-term-menu/ used for following code
 def show_program_menu():
     program_menu_title = "Select an option:"
     program_menu_items = ["View Statistics", "Add New Restaurant"]
@@ -35,19 +36,13 @@ def load_valid_values(sheet_name, column_index):
     return valid_values
 
 def view_statistics():
-    # Calls for owner's name input
     owner_name = get_valid_owner_name_input()
-
-    # Load existing data for owner names
     valid_owner_names = load_valid_values('survey_q', 1)
 
-    # Check if the entered owner's name already exists
     if owner_name in valid_owner_names:
         print(f"Owner's Name '{owner_name}' Welcome Back.")
-        # Call function to get food type input
-        food_type = get_food_type_input()
-        print(f"Food Type: {food_type}")
-        # Call function to get zip code input
+        # food_type = get_food_type_input()
+        # print(f"Food Type: {food_type}")
         zip_code = get_zip_code_input()
         print(f"Zip Code: {zip_code}")
 
@@ -101,7 +96,7 @@ def get_food_type_input():
     # Function to get valid food type input from the user
     while True:
         rest_type_input = input("Enter Food Type: ").strip()
-        
+
         # Validation code for food type input
         if any(d.isdigit() or not d.isalnum() for d in rest_type_input.lower()) or len(rest_type_input.strip()) == 0:
             print("Invalid input. Please enter a valid Food Type.")
@@ -118,6 +113,7 @@ def get_zip_code_input():
             print("Invalid input. Please enter a valid Zip Code.")
         else:
             return zip_code
+
 def add_new_restaurant():
     owner_name = get_valid_owner_name_input()
     rest_type = get_valid_rest_type_input()
@@ -145,15 +141,15 @@ def valid_owner_name_input(owner_input):
     # Allow letters and a single space, but not numbers or special characters
     if not all((o.isalpha() or (o.isspace() and owner_input.count(o) == 1)) for o in owner_input):
         return False
-    
+
     if len(owner_input.strip()) == 0:
         return False
-    
+
     return True
 
 def get_valid_owner_name_input():
     owner_input = input("Enter owner's name: ")
-    owner_input = owner_input.strip() 
+    owner_input = owner_input.strip()
 
     if valid_owner_name_input(owner_input):
         return owner_input
@@ -167,16 +163,16 @@ def validate_rest_type_input(rest_type_input):
     # When inputting your restaurant, no excess space, numbers, special characters (e.g., ()/&!)
     if any(d.isdigit() or not d.isalnum() for d in rest_type_input):
         return False
-    # .strip()) == 0: credited to Mentor 
-    if len(rest_type_input.strip()) == 0: 
+    # .strip()) == 0: credited to Mentor
+    if len(rest_type_input.strip()) == 0:
         return False
-    
+
     return True
 
 def get_valid_rest_type_input():
     while True:
         rest_type_input = input("Enter Restaurant Type: ")
-        rest_type_input = rest_type_input.strip()  
+        rest_type_input = rest_type_input.strip()
 
         if validate_rest_type_input(rest_type_input):
             return rest_type_input
@@ -187,10 +183,7 @@ def validate_zip_code(zip_code):
     # Check if the zip code is not empty
     if len(zip_code.strip()) == 0:
         return False
-
-    # Check if the zip code consists only digits
-    if not zip_code.isdigit():
-        return False
+    
 
     return True
 
